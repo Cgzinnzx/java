fb9badbb88114860048b9ce4160667d2dee0abf3
Cgzinnzx <144912107+Cgzinnzx@users.noreply.github.com>
2023-10-16 11:56:18 -0300

    Add files via upload

--- velocidade.js
+++ velocidade.js
@@ -1,25 +1,21 @@
 const form = document.querySelector("form")
-const resp1 = document.querySelector("h3")
-const resp2 = document.querySelector("h4")
+const resp = document.querySelector("h3")
 
 form.addEventListener ("submit",(e) =>{
     e.preventDefault()               
 
-        
-const VelocidadePermitida = Number(form.inVelocidadePermitida.value) 
-const VelocidadedoCondutor = Number(form.inVelocidadedoCondutor.value) 
-const  media= (nota1 + nota2)/2
-resp1.innerText = `Média das Notas  ${media.toFixed(2)}`
+    const Permitida = Number(form.inPermitida.value) 
+    const Condutor = Number(form.inCondutor.value) 
+    let leve = Permitida + (Permitida * 0.2)
+    alert(Permitida+Condutor+Leve)
 
-if(media >= 7) {
-    resp2.innerText = `Parabéns ${nome}! Você foi aprovado(a)`
-    resp2.style.color = "blue"
-} else if (media >=4) {
-    resp2.innerText = `Atenção ${nome}! Você está de exame`
-    resp2.style.color = "green"
-} else{
-    resp2.innerText = `Ops ${nome}! Você foi reprovado(a)`
-    resp2.style.color = "red"
-}
+    if(Condutor <= Permitida){
+    resp.innerText = `Situação: Sem Multa`
 
+    }  else if(Condutor <= Leve) {
+        resp.innerText = `Situação: Multa leve`
+
+    }  else {
+        resp.innerText = `Situação: Multa grave`
+    }
 })
